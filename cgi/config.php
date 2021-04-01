674dbba1d4e3f99a03026b281fbd2b3a6881c50a
Andrew Karnolt <akarnolt@vsu.edu>
2021-04-01 15:28:00 -0400

    Update config.php

--- cgi/config.php
+++ cgi/config.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'mediship_admin');
-define('DB_PASSWORD', 'Spring2021!');
-define('DB_NAME', 'mediship_login');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
